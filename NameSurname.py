1052ceb62e95847560dbdf8be265c3b39a79f573
araldortogul <araldortogul@gmail.com>
2022-12-20 20:48:29 +0300

    Fixed some bugs, added n=1 support

--- NameSurname.py
+++ NameSurname.py
@@ -2,7 +2,6 @@ from mpi4py import MPI
 import re
 import sys
 
-
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
@@ -39,78 +38,92 @@ if rank == 0:
 
     # Open input file
     text_file = open(args['input_file'])
-    bulk_data = text_file.read()    #read whole file to a string
+    bulk_data = text_file.read()    # read whole file to a string
     text_file.close()               # close file
     
-    sentences = re.split(r'\n+', bulk_data) # Get the list of sentences
-
-    parititonSize = len(sentences) // workerCount
-    rem = len(sentences) % workerCount # remainder
-
-    partitions = list() # Contains distributed data
-
-    # Distribute data evenly to workers
-    for i in range(workerCount):
-        partitions.append(sentences[(i* parititonSize):((i+1) * parititonSize)])
-    # Distribute the remainders
-    for i in range(rem):
-        partitions[i].append(sentences[parititonSize * workerCount + i])
-    
-    # Sending data to workers
-    for i, partition in enumerate(partitions):
-        comm.send(partition, dest = i+1, tag = 0)
-        comm.send(args['merge_method'], dest = i+1, tag = 1)
-    
-    ### Requirement 4:
+    sentences = re.findall(r'.+', bulk_data) # Get the list of sentences
 
     # Total unigram and bigram counts
     totalUnigramCount = dict()
     totalBigramCount = dict()
 
-    # Case 1
-    if args['merge_method'] == 'MASTER':
+    # If N > 1
+    if size > 1:
+        parititonSize = len(sentences) // workerCount
+        rem = len(sentences) % workerCount # remainder
 
-        for worker in range(1, workerCount + 1):
-            # Receive unigram and bigram counts from a worker
-            unigramCount = comm.recv(source = worker, tag = 2 * worker)
-            bigramCount  = comm.recv(source = worker, tag = 2 * worker + 1)
+        partitions = list() # Contains distributed data
 
-            # Merge unigrams counted by the worker
-            for unigram, count in unigramCount.items():
-                totalUnigramCount[unigram] = totalUnigramCount.get(unigram, 0) + count
-            
-            # Merge bigrams counted by the worker
-            for bigram, count in  bigramCount.items():
-                totalBigramCount[bigram] = totalBigramCount.get(bigram, 0) + count
+        # Distribute data evenly to workers
+        for i in range(workerCount):
+            partitions.append(sentences[(i* parititonSize):((i+1) * parititonSize)])
+        # Distribute the remainders
+        for i in range(rem):
+            partitions[i].append(sentences[parititonSize * workerCount + i])
+        
+        # Sending data to workers
+        for i, partition in enumerate(partitions):
+            comm.send(partition, dest = i+1, tag = 0)
+            comm.send(args['merge_method'], dest = i+1, tag = 1)
 
-        ## TODO: Erase these later    
-        #print ("TOTAL UNIGRAMS\n",totalUnigramCount)
-        #print ("TOTAL BIGRAMS\n", totalBigramCount)
+        # Case 1
+        if args['merge_method'] == 'MASTER':
 
-    # Case 2
-    elif args['merge_method'] == 'WORKERS':
-        # TODO: get unigram and bigram data from last worker (Send the unigram data with tag 2, bigram data with tag 3)
-        totalUnigramCount = comm.recv(source = workerCount, tag = 2 * workerCount)
-        totalBigramCount  = comm.recv(source = workerCount, tag = 2 * workerCount + 1)
-    
+            for worker in range(1, workerCount + 1):
+                # Receive unigram and bigram counts from a worker
+                unigramCount = comm.recv(source = worker, tag = 2 * worker)
+                bigramCount  = comm.recv(source = worker, tag = 2 * worker + 1)
+
+                # Merge unigrams counted by the worker
+                for unigram, count in unigramCount.items():
+                    totalUnigramCount[unigram] = totalUnigramCount.get(unigram, 0) + count
+                
+                # Merge bigrams counted by the worker
+                for bigram, count in  bigramCount.items():
+                    totalBigramCount[bigram] = totalBigramCount.get(bigram, 0) + count
+
+        # Case 2
+        elif args['merge_method'] == 'WORKERS':
+            # Receive unigram and bigram data from last worker
+            totalUnigramCount = comm.recv(source = workerCount, tag = 2 * workerCount)
+            totalBigramCount  = comm.recv(source = workerCount, tag = 2 * workerCount + 1)
+        
+        else:
+            raise Exception('Error: Unknown merge method.')
+
+    # If master process is the only process (there are no workers)
     else:
-        raise Exception('Error: Unknown merge method.')
+        print("Rank:", rank, "Number of sentences:", len(sentences))
+        # For each sentence in sentence list
+        for sentence in sentences:
 
-    # TODO: Requiement 4 (Calculating conditional probabilities)
+            # List of unigrams
+            unigramsOfSentence = re.findall(r'<s>|</s>|\w+', sentence)
 
-    text_file2 = open(args['test_file']) 
+            # Add unigrams to dictionary
+            for unigram in unigramsOfSentence:
+                totalUnigramCount[unigram] = totalUnigramCount.get(unigram, 0) + 1
+            
+            # List of bigrams
+            bigramsofSentence = list()
+            for i in range(len(unigramsOfSentence) - 1):
+                bigram = unigramsOfSentence[i] + " " + unigramsOfSentence[i+1]
+                totalBigramCount[bigram] = totalBigramCount.get(bigram, 0) + 1
+
+    # Requiement 4 (Calculating conditional probabilities)
+    text_file = open(args['test_file']) 
+    bulk_test_data = text_file.read()   # read whole file to a string
+    text_file.close()                   # close file
     
-    for line in text_file2:
-        temp = line[:-1]
-        a = temp.split(" ")[0]
-        b = temp.split(" ")[1]
-        denominator = totalUnigramCount[a]
-        nominator = totalBigramCount[temp]
-        cond_prob = nominator / denominator
-        str_cond_prob = str(cond_prob)
-        print ("P(" + b + "|" + a + ")= " + str_cond_prob)
+    test_bigrams = re.findall(r'.+', bulk_test_data) # Get the list of bigrams
 
-    text_file2.close()   
+    for bigram in test_bigrams:
+        [first, second] = bigram.split(" ")
+        denominator = totalUnigramCount.get(first,0)
+        nominator = totalBigramCount.get(bigram,0)
+        cond_prob = (nominator / denominator) if denominator != 0 else 0
+
+        print ("P(" + second + "|" + first + ")=", cond_prob)
 
 ### Worker process ###
 else:
@@ -151,22 +164,21 @@ else:
     # Case 2 
     elif merge_method == 'WORKERS':
 
-        # TODO: get data from previous worker (don't do this if rank == 1)
-
+        # Get data from previous worker (don't do this if rank == 1)
         if(rank > 1):
             previousWorker = rank - 1
             prevUnigramCount = comm.recv(source = previousWorker, tag = 2 * previousWorker)
             prevBigramCount  = comm.recv(source = previousWorker, tag = 2 * previousWorker + 1)
 
-        # TODO: merge this worker's data and the data obtained from the previous worker
-        for unigram, count in unigramCount.items():
-            unigramCount[unigram] = prevUnigramCount.get(unigram, 0) + count
+            # Merge unigrams counted by the previous worker
+            for unigram, count in prevUnigramCount.items():
+                unigramCount[unigram] = unigramCount.get(unigram, 0) + count
         
-        # Merge bigrams counted by the worker
-        for bigram, count in  bigramCount.items():
-            unigramCount[bigram] = prevBigramCount.get(bigram, 0) + count
+            # Merge bigrams counted by the previous worker
+            for bigram, count in  prevBigramCount.items():
+                bigramCount[bigram] = bigramCount.get(bigram, 0) + count
 
-        # TODO: send merged data to next worker (or to master if rank == workerCount)
+        # Send merged data to next worker (or to master if rank == workerCount)
         destination = 0 if rank == workerCount else (rank + 1)
         comm.send(obj = unigramCount, dest = destination, tag = 2 * rank)
         comm.send(obj = bigramCount,  dest = destination, tag = 2 * rank + 1)
